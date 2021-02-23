a798c97986d8a6eb3cb34fd906047b91ca91e899
hilbug <65197724+hilbug@users.noreply.github.com>
2021-02-22 23:37:06 -0500

    use async await

--- public/day10/script.js
+++ public/day10/script.js
@@ -6,19 +6,36 @@ jokeBtn.addEventListener('click', generateJoke);
 
 generateJoke();
 
-function generateJoke() {
+// USING ASYNC/AWAIT
+async function generateJoke() {
     const config = {
         headers: {
             'Accept': 'application/json'
         }
     }
 
-    fetch('https://icanhazdadjoke.com', config)
-        .then(response => response.json())
-        .then(data => {
-            jokeEl.innerHTML = data.joke;
-        });
+    const res = await fetch('https://icanhazdadjoke.com', config);
+
+    const data = await res.json();
+
+    jokeEl.innerHTML = data.joke;
+        
 }
+
+// USING .then()
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     }
+
+//     fetch('https://icanhazdadjoke.com', config)
+//         .then(response => response.json())
+//         .then(data => {
+//             jokeEl.innerHTML = data.joke;
+//         });
+// }
 
 
 
