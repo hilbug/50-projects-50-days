e55761e81159af625da2bff934c76b9174421e59
hilbug <65197724+hilbug@users.noreply.github.com>
2021-03-14 23:41:26 -0400

    finish up day13

--- public/day13/script.js
+++ public/day13/script.js
@@ -5,6 +5,14 @@ textarea.focus();
 
 textarea.addEventListener('keyup', (event) => {
     createTags(event.target.value);
+
+    if (event.key === "Enter") {
+        setTimeout(() => {
+            event.target.value = '';
+        }, 10);
+
+        randomSelect();
+    }
 })
 
 function createTags(input) {
@@ -18,4 +26,39 @@ function createTags(input) {
         tagEl.innerText = tag;
         tagsEl.appendChild(tagEl);
     });
+}
+
+function randomSelect() {
+    const times = 30; // number of times it highlights each one before it stops.
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag();
+
+        highlightTag(randomTag);
+
+        setTimeout(() => {
+            unHighlightTag(randomTag);
+        }, 100);
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval); // stops it and picks a random tag to highlight
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+            highlightTag(randomTag);
+        }, 100);
+    }, times * 100);
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll(".tag");
+    return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight');
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight');
 }
